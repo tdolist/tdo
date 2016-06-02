d7dedd7c10e5b84051d46c0ce96208379bc0abac
Felix Wittwer <dev@felixwittwer.de>
2016-06-02 12:05:58 +0200

    Add :clap:

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate clap;
+
+use clap::{Arg, App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("tdo")
+                          .version("0.0.1")
+                          .author("Felix Wittwer <dev@felixwittwer.de>, Felix Döring <mail@felixdoering.com>")
+                          .about("A todo list tool for the terminal")
+                          .get_matches();
 }

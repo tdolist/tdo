c20511687585678227fcf099711b345663b2535d
Felix Wittwer <dev@felixwittwer.de>
2017-04-04 19:44:28 +0200

    Expand cli yaml, rudimentary file loading

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,39 @@ extern crate tdo_core;
 
 #[allow(unused_imports)]
 use std::fs::File;
-use tdo_core::tdo;
+use tdo_core::{tdo,error};
 use clap::App;
 use std::env;
+use std::process::exit;
 
 
 #[allow(unused_variables)]
 fn main() {
+    // initialize the clap App handle
     let yml = load_yaml!("cli.yml");
-    let m = App::from_yaml(yml).version(crate_version!()).author(crate_authors!()).get_matches();
-    let location: String;
-    println!("{:?}", env::home_dir());
-    match env::home_dir() {
-        Some(path) => {
-            location = String::from(path.to_str().unwrap()) + "/.tdo/list.json";
-        },
-        None => location = ".".to_owned(),
-    }
-    println!("{:?}", &location);
-    let tdo = tdo::Tdo::load(&location).unwrap();
+    let matches = App::from_yaml(yml).version(crate_version!()).get_matches();
+
+    let save_path = match env::home_dir() {
+        Some(path) => path.join("/.tdo/list.json"),
+        None       => {
+            println!("[ERROR] You seem to have no home directory. Unfortunately this is required in order to use tdo.");
+            exit(1);
+        }
+    };
+    println!("[DEBUG] location: {:?}", &save_path);
+
+    let tdo = match tdo::Tdo::load(save_path.to_str().unwrap()) {
+        Ok(loaded)                              => loaded,
+        Err(error::ErrorKind::StorageError(error::StorageError::FileNotFound))  => tdo::Tdo::new(),
+        Err(error::ErrorKind::StorageError(error::StorageError::FileCorrupted)) => {
+            println!("[ERROR] The saved JSON could not be parsed. \
+            [ERROR] Please fix the saved json file manually or delete it to continue.");
+            exit(1);
+        }
+        Err(_) => panic!()
+    };
+
+
     tdo.save("./test.json").unwrap();
-    println!("{:?}", tdo);
+    println!("[DEBUG] tdo json content: {:?}", tdo);
 }

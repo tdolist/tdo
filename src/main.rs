65984f2a8ea47fa945738dfe226eb4105e1a80ec
h4llow3En <mail@felixdoering.com>
2017-04-04 16:23:21 +0200

    try something new or so

--- src/main.rs
+++ src/main.rs
@@ -7,35 +7,25 @@ extern crate tdo_core;
 
 #[allow(unused_imports)]
 use std::fs::File;
-use tdo_core::{todo, list, storage};
+use tdo_core::tdo;
 use clap::App;
+use std::env;
 
 
 #[allow(unused_variables)]
 fn main() {
     let yml = load_yaml!("cli.yml");
     let m = App::from_yaml(yml).version(crate_version!()).author(crate_authors!()).get_matches();
-    test();
-}
-
-#[allow(dead_code)]
-fn test() {
-    let mut tdo = list::Tdo::new();
-    let mut li = list::TodoList::new("kek");
-    let tdo1 = todo::Todo::new(0, "Bla");
-    let tdo2 = todo::Todo::new(1, "Bla1");
-
-    li.list.push(tdo1.clone());
-    li.list.push(tdo2);
-    tdo.lists.push(li);
-
-
-    let _ = tdo.save("foo.json");
-
-    // deserialisation
-
-    let mut new_tdo = list::Tdo::new();
-    new_tdo.load("foo.json");
-    println!("{:?}", &new_tdo);
-    println!("{:?}", &new_tdo.lists[0]);
+    let location: String;
+    println!("{:?}", env::home_dir());
+    match env::home_dir() {
+        Some(path) => {
+            location = String::from(path.to_str().unwrap()) + "/.tdo/list.json";
+        },
+        None => location = ".".to_owned(),
+    }
+    println!("{:?}", &location);
+    let tdo = tdo::Tdo::load(&location).unwrap();
+    tdo.save("./test.json").unwrap();
+    println!("{:?}", tdo);
 }

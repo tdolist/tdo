b91b2db0f74d288b28fe5f6d70fa984d81e5d80e
h4llow3En <mail@felixdoering.com>
2017-04-08 00:39:55 +0200

    update with conversion method for old list.json

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ extern crate tdo_core;
 
 #[allow(unused_imports)]
 use std::fs::File;
-use tdo_core::{tdo,error};
+use tdo_core::{tdo, error};
 use clap::App;
 use std::env;
 use std::process::exit;
@@ -20,23 +20,32 @@ fn main() {
     let matches = App::from_yaml(yml).version(crate_version!()).get_matches();
 
     let save_path = match env::home_dir() {
-        Some(path) => path.join("/.tdo/list.json"),
-        None       => {
-            println!("[ERROR] You seem to have no home directory. Unfortunately this is required in order to use tdo.");
+        Some(path) => path.join(".tdo/list.json"),
+        None => {
+            println!("[ERROR] You seem to have no home directory. Unfortunately this is required \
+                      in order to use tdo.");
             exit(1);
         }
     };
     println!("[DEBUG] location: {:?}", &save_path);
 
     let tdo = match tdo::Tdo::load(save_path.to_str().unwrap()) {
-        Ok(loaded)                              => loaded,
-        Err(error::ErrorKind::StorageError(error::StorageError::FileNotFound))  => tdo::Tdo::new(),
+        Ok(loaded) => loaded,
+        Err(error::ErrorKind::StorageError(error::StorageError::FileNotFound)) => tdo::Tdo::new(),
         Err(error::ErrorKind::StorageError(error::StorageError::FileCorrupted)) => {
-            println!("[ERROR] The saved JSON could not be parsed. \
+            println!("[ERROR] The saved JSON could not be parsed.\n\
             [ERROR] Please fix the saved json file manually or delete it to continue.");
             exit(1);
         }
-        Err(_) => panic!()
+        Err(error::ErrorKind::StorageError(error::StorageError::UnableToConvert)) => {
+            println!("[ERROR] The File could not be converted to the new version automatically.\n\
+            [ERROR] Please fix the saved json file manually or delete it to continue.");
+            exit(1);
+        }
+        Err(e) => {
+            println!("{:?}", e);
+            panic!();
+        }
     };
 
 
